2a20f9d6d8826d72bf89fc5104949595a56e5d88
Axel Sepúlveda <ansepulveda@uc.cl>
2022-01-07 12:07:16 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 blog_title = 'Read the Docs Blog'
-blog_baseurl = 'https://blog.readthedocs.com'
+blog_baseurl = 'https://github.com/chepo92/read-the-docs-blog'
 blog_path = 'archive'
 fontawesome_link_cdn = 'https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css'
 # fontawesome_included = True
